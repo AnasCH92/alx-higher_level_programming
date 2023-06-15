02d513dc87ee7428c5115b15fdd955c30d3513c5
Anas Chakir <anas_chakir@outlook.com>
2023-06-15 13:09:17 -0700

    task 0

--- 0x04-Python-More_Data_Structures/0-square_matrix_simple.py
+++ 0x04-Python-More_Data_Structures/0-square_matrix_simple.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    ret = []
-    for row in matrix:
-        newRow = list(map(lambda x: x * x, row))
-        ret.append(newRow)
-    return ret
+def calculate_square_matrix(matrix=[]):
+    # Create a new matrix of the same size as the input matrix
+    square_matrix = [[0 for _ in range(len(row))] for row in matrix]
+    
+    # Iterate over each element in the input matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            # Compute the square of the current element and assign it to the corresponding position in the square matrix
+            square_matrix[i][j] = matrix[i][j] ** 2
+    
+    return square_matrix
