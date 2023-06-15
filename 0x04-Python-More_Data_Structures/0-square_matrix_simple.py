c06beef31b305af1a6d42de0d2fe36f8c62ec647
Anas Chakir <anas_chakir@outlook.com>
2023-06-15 12:35:49 -0700

    tasks updatesé

--- 0x04-Python-More_Data_Structures/0-square_matrix_simple.py
+++ 0x04-Python-More_Data_Structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    ret = []
+    for row in matrix:
+        newRow = list(map(lambda x: x * x, row))
+        ret.append(newRow)
+    return ret
--- 0x04-Python-More_Data_Structures/1-search_replace.py
+++ 0x04-Python-More_Data_Structures/1-search_replace.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cp = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            cp.append(replace)
-        else:
-            cp.append(my_list[i])
-    return cp
+    newList = list(map(lambda x: x, my_list))
+    for i in range(len(newList)):
+        newList[i] = replace if newList[i] == search else newList[i]
+    return newList
